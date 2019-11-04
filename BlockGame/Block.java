9996cbd0e226077160421e810648ac3832548ff8
Joe O'Regan <joeaoregan@yahoo.ie>
2019-11-04 20:40:24 +0000

    Block Game - Random Ball Direction, Font Size

--- BlockGame/Block.java
+++ BlockGame/Block.java
@@ -3,16 +3,24 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.util.Random;
 
 public class Block extends Rectangle{
+	final int DIFFERENCE=3;
+
 	Image pic;
-	int dx=3;
-	int dy=-3;
+	int dx=DIFFERENCE;
+	int dy=-DIFFERENCE;
 	Rectangle left, right;
-	boolean destroyed=false;
-	boolean powerup=false;
+	private boolean destroyed=false;
+	private boolean powerup=false;
+
+	Random random=new Random();
 
 	Block(int a, int b, int w, int h, String s){
+		if(random.nextInt(10000)%2==0){
+			dx*=-1;	// Set random start direction for Ball
+		}
 		x=a;
 		y=b;
 		width=w;
@@ -26,5 +34,21 @@ public class Block extends Rectangle{
 		if(!destroyed){
 			g.drawImage(pic,x,y,width,height,c);
 		}
+	}
+
+	public boolean getDestroyed(){
+		return destroyed;
+	}
+
+	public void setDestroyed(boolean destroyed){
+		this.destroyed=destroyed;
+	}
+
+	public boolean getPowerup(){
+		return powerup;
+	}
+
+	public void setPowerup(boolean powerup){
+		this.powerup=powerup;
 	}
 }
--- BlockGame/BlockBreakerPanel.java
+++ BlockGame/BlockBreakerPanel.java
@@ -1,14 +1,15 @@
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Font;
 import javax.swing.JPanel;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class BlockBreakerPanel extends JPanel implements KeyListener{
 	ArrayList<Block> blocks = new ArrayList<Block>();
-	ArrayList<Block> ball = new ArrayList<Block>();
-	ArrayList<Block> powerup = new ArrayList<Block>();
+	ArrayList<Block> balls = new ArrayList<Block>();
+	ArrayList<Block> powerups = new ArrayList<Block>();
 	Block paddle;
 	Thread thread;
 	Animate animate;
@@ -25,72 +26,75 @@ public class BlockBreakerPanel extends JPanel implements KeyListener{
 			blocks.add(new Block(i*60+2,75,60,25,"yellow.png"));
 		}
 		Random random=new Random();
-		blocks.get(random.nextInt(32)).powerup=true;
-		blocks.get(random.nextInt(32)).powerup=true;
-		blocks.get(random.nextInt(32)).powerup=true;
-		blocks.get(random.nextInt(32)).powerup=true;
-		blocks.get(random.nextInt(32)).powerup=true;
+		blocks.get(random.nextInt(32)).setPowerup(true);
+		blocks.get(random.nextInt(32)).setPowerup(true);
+		blocks.get(random.nextInt(32)).setPowerup(true);
+		blocks.get(random.nextInt(32)).setPowerup(true);
+		blocks.get(random.nextInt(32)).setPowerup(true);
 
-		ball.add(new Block(237,437,25,25,"ball.png"));
+		balls.add(new Block(237,437,25,25,"ball.png"));
 		addKeyListener(this);
 		setFocusable(true);
 	}
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		for(Block b : blocks){
-			b.draw(g,this);
+		for(Block block : blocks){
+			block.draw(g,this);
 		}
-		for(Block b : ball){
-			b.draw(g,this);
+		for(Block ball : balls){
+			ball.draw(g,this);
 		}
-		for(Block p : powerup){
-			p.draw(g,this);
+		for(Block powerup : powerups){
+			powerup.draw(g,this);
 		}
 		paddle.draw(g, this);
 
 		g.drawString("Score: "+score, 10, getHeight()-10);	// Score
 
 		if(!gameStarted){
+			g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 20));
+
 			String pressEnter="Press Enter To Begin";
 			int textWidth=g.getFontMetrics().stringWidth(pressEnter);
-			g.drawString(pressEnter, (getWidth()/2)-(textWidth/2), getHeight()-10);	// Start Game
+
+			g.drawString(pressEnter, (getWidth()/2)-(textWidth/2), getHeight()/2);	// Start Game
 		}
 	}
 
 	public void update(){
-		for(Block p:powerup){
-			p.y+=1;	// drop powerup
-			if(p.intersects(paddle)&& !p.destroyed){
-				p.destroyed=true;
+		for(Block powerup:powerups){
+			powerup.y+=1;	// drop powerup
+			if(powerup.intersects(paddle)&& !powerup.getDestroyed()){
+				powerup.setDestroyed(true);
 				score+=25;
-				ball.add(new Block(paddle.dx+75,437,25,25,"ball.png"));
+				balls.add(new Block(paddle.x+75,437,25,25,"ball.png"));
 			}
 		}
 
-		for(Block ba:ball){
-			ba.x+=ba.dx;
-			if(ba.x>(getWidth()-size)&&ba.dx>0 || ba.x<0){
-				ba.dx*=-1;
+		for(Block ball:balls){
+			ball.x+=ball.dx;
+			if(ball.x>(getWidth()-size)&&ball.dx>0 || ball.x<0){
+				ball.dx*=-1;
 			}
-			if(ba.y<0 || ba.intersects(paddle)){
-				ba.dy*=-1;
+			if(ball.y<0 || ball.intersects(paddle)){
+				ball.dy*=-1;
 			}
-			ba.y+=ba.dy;
+			ball.y+=ball.dy;
 
-			for(Block b:blocks){
-				if((b.left.intersects(ba)||b.right.intersects(ba)) && !b.destroyed){
-					ba.dx*=-1;
-					b.destroyed=true;
-					if(b.powerup){
-						powerup.add(new Block(b.x,b.y,25,19,"extra.png"));
+			for(Block block:blocks){
+				if((block.left.intersects(ball)||block.right.intersects(ball)) && !block.getDestroyed()){
+					ball.dx*=-1;
+					block.setDestroyed(true);
+					if(block.getPowerup()){
+						powerups.add(new Block(block.x,block.y,25,19,"extra.png"));
 					}
-				}else if(ba.intersects(b) && !b.destroyed){
-					b.destroyed=true;
+				}else if(ball.intersects(block) && !block.getDestroyed()){
+					block.setDestroyed(true);
 					score+=10;
-					ba.dy*=-1;
-					if(b.powerup){
-						powerup.add(new Block(b.x,b.y,25,19,"extra.png"));
+					ball.dy*=-1;
+					if(block.getPowerup()){
+						powerups.add(new Block(block.x,block.y,25,19,"extra.png"));
 					}
 				}
 			}
@@ -113,8 +117,7 @@ public class BlockBreakerPanel extends JPanel implements KeyListener{
 		}
 		if(e.getKeyCode()==KeyEvent.VK_LEFT && paddle.x>0){
 			paddle.x-=15;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT && paddle.x<(getWidth()-paddle.width)){
+		}else if(e.getKeyCode()==KeyEvent.VK_RIGHT && paddle.x<(getWidth()-paddle.width)){
 			paddle.x+=15;
 		}
 	}
